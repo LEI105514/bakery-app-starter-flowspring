da51dd9fffaefcb34cfd19bb293d4dc22b25cbe3
gavmo <gavmo@iscte-iul.pt>
2025-09-25 11:09:35 +0100

    Documentação javadoc para a Classe DashboardData.java

--- src/main/java/com/vaadin/starter/bakery/backend/data/DashboardData.java
+++ src/main/java/com/vaadin/starter/bakery/backend/data/DashboardData.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import com.vaadin.starter.bakery.backend.data.entity.Product;
 
+/**
+ * Controls the data shown in the dashboard (gets and sets)
+ */
 public class DashboardData {
 
 	private DeliveryStats deliveryStats;
@@ -13,42 +16,83 @@ public class DashboardData {
 	private Number[][] salesPerMonth;
 	private LinkedHashMap<Product, Integer> productDeliveries;
 
+    /**
+     *
+     * @return
+     */
 	public DeliveryStats getDeliveryStats() {
 		return deliveryStats;
 	}
 
+    /**
+     *
+     * @param deliveryStats
+     */
 	public void setDeliveryStats(DeliveryStats deliveryStats) {
 		this.deliveryStats = deliveryStats;
 	}
 
+    /**
+     *
+     * @return
+     */
 	public List<Number> getDeliveriesThisMonth() {
 		return deliveriesThisMonth;
 	}
 
+    /**
+     *
+     * @param deliveriesThisMonth
+     */
 	public void setDeliveriesThisMonth(List<Number> deliveriesThisMonth) {
 		this.deliveriesThisMonth = deliveriesThisMonth;
 	}
 
+    /**
+     *
+     * @return
+     */
 	public List<Number> getDeliveriesThisYear() {
 		return deliveriesThisYear;
 	}
 
+    /**
+     *
+     * @param deliveriesThisYear
+     */
 	public void setDeliveriesThisYear(List<Number> deliveriesThisYear) {
 		this.deliveriesThisYear = deliveriesThisYear;
 	}
 
+    /**
+     *
+     * @param salesPerMonth
+     */
 	public void setSalesPerMonth(Number[][] salesPerMonth) {
 		this.salesPerMonth = salesPerMonth;
 	}
 
+    /**
+     *
+     * @param i
+     * @return
+     */
 	public Number[] getSalesPerMonth(int i) {
 		return salesPerMonth[i];
 	}
 
+    /**
+     *
+     * @return
+     */
 	public LinkedHashMap<Product, Integer> getProductDeliveries() {
 		return productDeliveries;
 	}
 
+    /**
+     *
+     * @param productDeliveries
+     */
 	public void setProductDeliveries(LinkedHashMap<Product, Integer> productDeliveries) {
 		this.productDeliveries = productDeliveries;
 	}
